a6c92ae8ef16fd592845e03b1103046ccfbf5ddd
andrej <andrej.batarilo@reprisk.com>
2024-03-03 13:27:58 +0100

    JWT authentication fixed

--- user-service/src/main/java/com/batarilo/userservice/service/JwtService.java
+++ user-service/src/main/java/com/batarilo/userservice/service/JwtService.java
@@ -2,24 +2,26 @@ package com.batarilo.userservice.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.function.Function;
+
 @Service
-public class JwtService {
+public class JwtService
+{
     @Value("${security.jwt.public-key}")
     private String publicKeyString;
 
@@ -29,84 +31,94 @@ public class JwtService {
     @Value("${security.jwt.expiration-time}")
     private long jwtExpiration;
 
-    public String extractUsername(String token) {
+    public String extractUsername(String token)
+    {
         return extractClaim(token, Claims::getSubject);
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver)
+    {
+        final Claims claims;
+        try {
+            claims = extractAllClaims(token);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        }
         return claimsResolver.apply(claims);
     }
 
-    public String generateToken(UserDetails userDetails) {
-        return generateToken(new HashMap<>(), userDetails);
+    public String generateToken(UserDetails userDetails)
+    {
+        return buildToken(userDetails);
     }
 
-    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return buildToken(extraClaims, userDetails, jwtExpiration);
-    }
-
-    public long getExpirationTime() {
+    public long getExpirationTime()
+    {
         return jwtExpiration;
     }
 
     private String buildToken(
-        Map<String, Object> extraClaims,
-        UserDetails userDetails,
-        long expiration
-    ) {
+        UserDetails userDetails
+    )
+    {
         PrivateKey privateKey = getPrivateKey();
-        return Jwts
-            .builder()
-            .setClaims(extraClaims)
+
+        Instant now = Instant.now();
+
+        return Jwts.builder()
             .setSubject(userDetails.getUsername())
-            .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + expiration))
-            .signWith(privateKey, SignatureAlgorithm.RS256)
+            .setAudience("account-d.docusign.com")
+            .claim("scope", "signature impersonation")
+            .setIssuedAt(Date.from(now))
+            .setExpiration(Date.from(now.plus(5l, ChronoUnit.MINUTES)))
+            .signWith(privateKey)
             .compact();
     }
 
-    public boolean isTokenValid(String token, UserDetails userDetails) {
+    public boolean isTokenValid(String token, UserDetails userDetails)
+    {
         final String username = extractUsername(token);
         return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
     }
 
-    private boolean isTokenExpired(String token) {
+    private boolean isTokenExpired(String token)
+    {
         return extractExpiration(token).before(new Date());
     }
 
-    private Date extractExpiration(String token) {
+    private Date extractExpiration(String token)
+    {
         return extractClaim(token, Claims::getExpiration);
     }
 
-    private Claims extractAllClaims(String token) {
+    private Claims extractAllClaims(String token) throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
         return Jwts
             .parserBuilder()
-            .setSigningKey(getPublicKey())
+            .setSigningKey(getPrivateKey())
             .build()
             .parseClaimsJws(token)
             .getBody();
     }
 
-    private PrivateKey getPrivateKey() {
-        try {
-            byte[] keyBytes = Base64.decodeBase64(privateKeyString);
-            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            return keyFactory.generatePrivate(spec);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load private key", e);
-        }
-    }
+    private PrivateKey getPrivateKey()
+    {
 
-    private PublicKey getPublicKey() {
+        String rsaPrivateKey = privateKeyString.replace("-----BEGIN PRIVATE KEY-----", "");
+        rsaPrivateKey = rsaPrivateKey.replace("-----END PRIVATE KEY-----", "");
+
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(java.util.Base64.getDecoder().decode(rsaPrivateKey));
+        KeyFactory kf = null;
         try {
-            byte[] keyBytes = Base64.decodeBase64(publicKeyString);
-            X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
-            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-            return keyFactory.generatePublic(spec);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to load public key", e);
+            kf = KeyFactory.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            return kf.generatePrivate(keySpec);
+        } catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
         }
     }
 }
